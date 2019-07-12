f6711390675ffdfef6982259d2c43f2123616bfc
IrsantyoHadi <irsantyohadi@gmail.com>
2019-07-12 08:51:45 +0700

    beresin tampilan dikit, kalo mau dirubah sesuai temanya gw ikut aja..

--- recipe.js
+++ recipe.js
@@ -2,7 +2,7 @@ let detailrecipe = []
 $(document).ready(function () {
   getRepo()
 })
-function getRepo(){
+function getRepo() {
   $('#foodbutton').click(function () {
     axios.post("http://localhost:3000/apiRecipe", {
       foodName: $("#food").val()
@@ -12,10 +12,15 @@ function getRepo(){
         $("#listrecipe").empty()
         for (let i = 0; i < data.hits.length; i++) {
           $("#listrecipe").append(`
-          <br>
-          <img src="${data.hits[i].recipe.image}" alt="no image" width="100" height="100">
-          <p onclick= "detail('${data.hits[i].recipe.label}')" >${data.hits[i].recipe.label}</p>
-          <p id="${i}-detail-recipe"></p>
+          <div class="d-inline-flex">
+          <div class="container>
+          <div class="card" style="width: 300px" style="height: 200px">
+          <img class="card-img-top" src="${data.hits[i].recipe.image}" alt="no image">
+          <h4 class="card-title" onclick= "detail('${data.hits[i].recipe.label}')" >${data.hits[i].recipe.label}</h4>
+          <p class="card-text" id="${i}-detail-recipe"></p>
+          </div>
+          </div>
+          </div>
           `)
         }
       })
@@ -23,20 +28,17 @@ function getRepo(){
         console.log(err);
       })
   });
-  
+
 }
-    function detail(name) {
-      console.log(name)
-      console.log(detailrecipe);
-      for (let i = 0; i < detailrecipe.length; i++) {
-        if (detailrecipe[i].recipe.label == name) {
-          console.log(detailrecipe[i].recipe.ingredientLines)
-          detailrecipe[i].recipe.ingredientLines.forEach(element => {
-            console.log(element);
-            $(`#${i}-detail-recipe`).append(`
+function detail(name) {
+  for (let i = 0; i < detailrecipe.length; i++) {
+    $(`#${i}-detail-recipe`).empty()
+    if (detailrecipe[i].recipe.label == name) {
+      detailrecipe[i].recipe.ingredientLines.forEach(element => {
+        $(`#${i}-detail-recipe`).append(`
               <li> ${element}</li>
             `)
-          });
-        }
-      }
+      });
     }
+  }
+}
